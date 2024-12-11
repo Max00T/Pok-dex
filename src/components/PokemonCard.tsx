b368e90dd6d012e95cc1c8d305de78b259ed11bf
Max00T <trousselmaxime@gmail.com>
2024-12-11 16:21:38 +0100

    react Basic 3

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,26 @@
 function PokemonCard() {
+  const pokemon = pokemonList[0];
+
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="pokemon Bulbasaur "
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
     </figure>
   );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 export default PokemonCard;
